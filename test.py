3ee3782fce9d5218953f3cb5c47129d5f81ef4a2
jamedrano <jamedrano@uvg.edu.gt>
2024-06-10 16:24:48 -0600

    Update test.py

--- test.py
+++ test.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 import streamlit as st
 import io
 import pygwalker as pyg
+from io import BytesIO
+from pyxlsb import open_workbook as open_xlsb
+
 
 from xgboost import XGBRegressor
 from xgboost import XGBClassifier
@@ -135,7 +138,22 @@ if uploaded_file is not None:
     respuesta = 'R28D'
     desplegar()
    
+  with tab5:
    
+   def to_excel(df):
+    output = BytesIO()
+    writer = pd.ExcelWriter(output, engine='xlsxwriter')
+    df.to_excel(writer, index=False, sheet_name='Sheet1')
+    workbook = writer.book
+    worksheet = writer.sheets['Sheet1']
+    format1 = workbook.add_format({'num_format': '0.00'}) 
+    worksheet.set_column('A:A', None, format1)  
+    writer.save()
+    processed_data = output.getvalue()
+    return processed_data
+    
+   df_xlsx = to_excel(datosprueba)
+   st.download_button(label='📥 Download Current Result',data=df_xlsx ,file_name= 'df_test.xlsx')
    
 
    
