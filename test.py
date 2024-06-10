c9460a9d5e3cbfa97a3f11fe3390c037da43787f
jamedrano <jamedrano@uvg.edu.gt>
2024-06-10 16:30:10 -0600

    Update test.py

--- test.py
+++ test.py
@@ -152,7 +152,7 @@ if uploaded_file is not None:
     processed_data = output.getvalue()
     return processed_data
     
-   df_xlsx = to_excel(datosprueba)
+   df_xlsx = to_excel(subdatos2)
    st.download_button(label='📥 Download Current Result',data=df_xlsx ,file_name= 'df_test.xlsx')
    
 
